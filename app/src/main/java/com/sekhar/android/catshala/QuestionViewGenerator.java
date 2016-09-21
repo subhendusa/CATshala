fe9b82a4997abda6f7d82b7f5eba193e427afbfb
Subhendu Sekhar Sahoo <subhendu.sekhar@gmail.com>
2016-09-22 04:01:52 +0530

    Updated ListView with RecyclerView

--- app/src/main/java/com/sekhar/android/catshala/QuestionViewGenerator.java
+++ app/src/main/java/com/sekhar/android/catshala/QuestionViewGenerator.java
@@ -1,13 +1,13 @@
 package com.sekhar.android.catshala;
 
 import android.app.Activity;
-import android.content.Context;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -30,12 +30,14 @@ public class QuestionViewGenerator {
     }
 
     private void createQuestionsView() {
-        ListView listView = (ListView) activity.findViewById(R.id.questions_list_id);
+        RecyclerView recyclerView = (RecyclerView) activity.findViewById(R.id.questions_list_id);
         List<Question> questions = new QuestionReader().getQuestions(activity, 1L);
 
-        QuestionAdapter adapter = new QuestionAdapter(activity, R.layout.question,
-                questions);
-        listView.setAdapter(adapter);
+        QuestionAdapter adapter = new QuestionAdapter(questions);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(activity);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
 
         adapter.notifyDataSetChanged();
     }
@@ -44,46 +46,50 @@ public class QuestionViewGenerator {
     }
 }
 
-class QuestionAdapter extends ArrayAdapter<Question> {
-    Context context;
+class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.MyViewHolder> {
 
-    public QuestionAdapter(Context context, List<Question> questions) {
-        super(context, 0, questions);
-        this.context = context;
-    }
+    private List<Question> moviesList;
 
-    public QuestionAdapter(Context context, int resource, List<Question> questions) {
-        super(context, resource, questions);
-        this.context = context;
+    public QuestionAdapter(List<Question> moviesList) {
+        this.moviesList = moviesList;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.question, parent, false);
 
-        // Get the data item for this position
-        Question question = getItem(position);
+        return new MyViewHolder(itemView);
+    }
 
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.question, parent, false);
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Question question = moviesList.get(position);
+
+        holder.tvQuestion.setText(question.getQuestionNo() + ". " + question.getText());
+        holder.cbOption1.setText(question.getOptions().get(0).getText());
+        holder.cbOption2.setText(question.getOptions().get(1).getText());
+        holder.cbOption3.setText(question.getOptions().get(2).getText());
+        holder.cbOption4.setText(question.getOptions().get(3).getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvQuestion;
+        public CheckBox cbOption1, cbOption2, cbOption3, cbOption4;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            tvQuestion = (TextView) view.findViewById(R.id.question_text_id);
+            cbOption1 = (CheckBox) view.findViewById(R.id.option1);
+            cbOption2 = (CheckBox) view.findViewById(R.id.option2);
+            cbOption3 = (CheckBox) view.findViewById(R.id.option3);
+            cbOption4 = (CheckBox) view.findViewById(R.id.option4);
         }
-
-        // Lookup view for data population
-        TextView tvQuestion = (TextView) convertView.findViewById(R.id.question_text_id);
-        CheckBox cbOption1 = (CheckBox) convertView.findViewById(R.id.option1);
-        CheckBox cbOption2 = (CheckBox) convertView.findViewById(R.id.option2);
-        CheckBox cbOption3 = (CheckBox) convertView.findViewById(R.id.option3);
-        CheckBox cbOption4 = (CheckBox) convertView.findViewById(R.id.option4);
-
-        // Populate the data into the template view using the data object
-        tvQuestion.setText(question.getQuestionNo() + ". " + question.getText());
-        cbOption1.setText(question.getOptions().get(0).getText());
-        cbOption2.setText(question.getOptions().get(1).getText());
-        cbOption3.setText(question.getOptions().get(2).getText());
-        cbOption4.setText(question.getOptions().get(3).getText());
-
-        // Return the completed view to render on screen
-        return convertView;
     }
 }
-
